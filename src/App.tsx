9f06df3630f146c0dbc81a8e22a73cd735d15069
Mithilesh Gupta <mithileshgupta059@gmail.com>
2024-05-05 12:02:45 +0530

    Added code to fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,34 @@
+import {useEffect} from "react";
+
 function App() {
+  // Functions
+  const fetchJobs = async () => {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const body = JSON.stringify({
+      "limit": 10,
+      "offset": 0,
+    })
+
+    const requestOptions = {
+      method: "POST",
+      headers,
+      body,
+    }
+
+    return await fetch('https://api.weekday.technology/adhoc/getSampleJdJSON', requestOptions);
+  }
+
+  // Lifecycle functions
+  useEffect(() => {
+    fetchJobs().then(jobs => {
+      return jobs.json();
+    }).then(jobs => {
+      console.log(jobs);
+    });
+  }, []);
+
   return (
     <>
     </>
